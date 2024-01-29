f0640208c2909235a2e89258150bfeb5a2a99413
莉雄樹 佐藤 <220350@jc-21.jp>
2024-01-29 10:10:22 +0900

    PSOs

--- ColocEngine/PSOManager.h
+++ ColocEngine/PSOManager.h
@@ -1,7 +1,29 @@
 #pragma once
 #include"PSO.h"
+#include<d3d12.h>
+#include<array>
 
 namespace PSOManager
 {
-};
+	void Init();
 
+	enum class Shader3D
+	{
+		Default,
+		Toon,
+
+		AMMOUNT
+	};
+
+	enum class ShaderPost
+	{
+		Default,
+
+		AMMOUNT
+	};
+
+	extern std::array<ID3D12PipelineState*, static_cast<uint32_t>(Shader3D::AMMOUNT)> PSO3D;
+	extern std::array<ID3D12PipelineState*, static_cast<uint32_t>(ShaderPost::AMMOUNT)> PSOPost;
+
+	ID3D12PipelineState* 
+};
